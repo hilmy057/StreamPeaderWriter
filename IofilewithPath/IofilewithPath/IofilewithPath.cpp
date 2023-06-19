65b69bcd4c01f0f929a372bd64cb24b8caf1d52d
hilmy057 <naufalh702@gmail.com>
2023-06-19 10:57:39 +0700

    commit 1

---
+++ IofilewithPath/IofilewithPath/IofilewithPath.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "masukkan nama file : "; 
+	cin >> NamaFile;
+
+	ofstream outfile;
+
+	outfile.open(NamaFile, ios::out);
+
+	cout << ">= Menulis file,
+}
+
+ 
