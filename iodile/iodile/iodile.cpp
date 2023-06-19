dd14b1e7398d71a458d762cd7aaf49df67cc4945
hilmy057 <naufalh702@gmail.com>
2023-06-19 10:50:12 +0700

    commit 1

---
+++ iodile/iodile/iodile.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+
+	ofstream outfile;
+
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	while (true) {
+		cout << ",,";
+
+		getline(cin, baris);
+
+		if (baris == "q") break;
+	}
+}
